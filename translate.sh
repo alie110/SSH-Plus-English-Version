f9a83daeb5c57f29efa953578fc04724cae6a6c4
alie110 <118800838+alie110@users.noreply.github.com>
2023-04-09 00:39:37 +0330

    Update translate.sh

--- translate.sh
+++ translate.sh
@@ -2,8 +2,7 @@
 clear
 echo ""
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[0;32m-        TRANSLATE BY PHC_MARED0 PHC_Jayvee      -\033[0m"
-echo -e "\033[0;32m-              REQUESTED BY CUPCAKE_13           -\033[0m"
+echo -e "\033[0;32m-            IRAN - AETeam - SSH PLUS            -\033[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 sleep 2
@@ -14,75 +13,75 @@ sleep 2
 echo "Please Wait..............."
 echo ""
 echo -ne '[#                       ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/addhost -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/addhost -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[##                      ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/alterarlimite -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/alterarlimite -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[###                     ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/alterarsenha -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/alterarsenha -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[####                    ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/attscript -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/attscript -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[#####                   ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/badvpn -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/badvpn -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[######                  ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/banner -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/banner -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[#######                 ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/blockt -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/blockt -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[########                ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/botssh -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/botssh -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[#########               ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/conexao -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/conexao -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[##########              ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/criarteste -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/criarteste -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[###########             ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/criarusuario -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/criarusuario -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[############            ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/delhost -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/delhost -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[#############           ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/delscript -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/delscript -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[##############          ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/detalhes -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/detalhes -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[###############         ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/expcleaner -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/expcleaner -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[################        ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/infousers -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/infousers -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[#################       ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/menu -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/menu -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[##################      ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/mudardata -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/mudardata -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[###################     ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/otimizar -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/otimizar -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[####################    ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/reiniciarservicos -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/reiniciarservicos -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[#####################   ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/reiniciarsistema -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/reiniciarsistema -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[######################  ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/remover -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/remover -P /tmp/SSH-PLUS-ENG/
 sleep 0.3
 echo -ne '[####################### ]\r'
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/senharoot -P /tmp/SSH-PLUS-ENG/
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/sshmonitor -P /tmp/SSH-PLUS-ENG/
-wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/userbackup -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/senharoot -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/sshmonitor -P /tmp/SSH-PLUS-ENG/
+wget -q https://raw.githubusercontent.com/alie110/SSH-Plus-English-Version/main/cmd/userbackup -P /tmp/SSH-PLUS-ENG/
 echo -ne '[########################]\r'
 echo ""
 echo ""
@@ -100,4 +99,3 @@ sleep 2
 echo "exiting................"
 echo ""
 sleep 3
-
